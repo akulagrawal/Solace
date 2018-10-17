f5cb90c5443198b0bf6808c1bfcb83eca06e3c8b
Pravandan <pravandan.chand@gmail.com>
2018-10-17 19:41:32 +0530

    Linking of the static web pages

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index_view(request):
+    return render(request,'main/search_box.html')
+
+def search_view(request):
+    return render(request,'main/enter_city.html')
+
+def results_view(request):
+    return render(request,'main/result.html')
